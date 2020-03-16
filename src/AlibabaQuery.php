164440e503281279f9e75f26ec5f498c35557e6f
Foggy <505104451@qq.com>
2020-03-16 13:47:20 +0800

    完善了alibaba-query类

--- src/AlibabaQuery.php
+++ src/AlibabaQuery.php
@@ -16,61 +16,27 @@ class AlibabaQuery {
 	public function run(){
 		$this->getInputUrl();
 		$html = QueryList::get($this->inputUrl);
-		$urlParams = parse_url($this->inputUrl);
-		$domain = $urlParams['scheme'].'://'.$urlParams['host'];
-		//获取到结果一共有多少页
-		$urls = [];
 		$count = $html->find('.next-pagination-list a:eq(-1)')->text();
 		if(empty($count)){
-			die('搜索失败，请手动检索此关键字下是否有商品');
+			print '搜索失败，请手动检索此关键字下是否有商品';
+			exit;
 		}
-		$url = $html->find('.next-pagination-list a:eq(-1)')->href;
-		$url = $domain.$url;
-		$param = parse_url($url);
-		$ex = explode('/', $param['path']);
-		array_pop($ex);
-		$newParam = implode('/', $ex);
-		//var_dump($newParam);exit;
-		$param['path'] = $newParam;
-		for($i = 1; $i<= $count; $i++){
-			$urls[$i] = $param['scheme'].'://'.$param['host'].$param['path'].'/'.$i.'.html';
-		}
-		print "------------------------------\r\n";
-		print '搜索成功，本次搜索一共有'.count($urls).'页'."\r\n";
-		print "------------------------------\r\n";
 
+		$urls = $this->urlFormat();
+		
 		$data = [];
 
 		QueryList::multiGet($urls)->success(function(QueryList $ql,Response $response, $index) use($urls, &$data){
 			$url = $urls[$index+1];
 			$listHtml = $ql->get($url);
 			$title = $listHtml->find('.component-product-list .product-item .product-info a')->attrs('title')->all();
-			//$link = $listHtml->find('.component-product-list .product-item .product-info a')->attrs('href');
 			$data[$index+1] = $title;
 		})->send();
 
 		print "数据采集成功，正在格式化处理...\r\n";
 
 		//打印结果
-		$string = '';
-		ksort($data);
-		foreach ($data as $key => $value) {
-			$string .= '--------第'.$key.'页--------'."\r\n\r\n";
-			print '--------第'.$key.'页--------'."\r\n";;
-			foreach ($value as $k => $v) {
-				$string .= ($k+1).':'.$v."\r\n\r\n";
-				print ($k+1).':'.$v."\r\n";
-			}
-			print "\r\n";
-		}
-		$time = date('Y-m-d-H-i-s', time());
-		$file = './data/'.$time.'.txt';
-		if(!is_dir('./data')){
-			mkdir('./data/', 0755, true);
-		}
-		file_put_contents($file, $string);
-		print "------------------------------\r\n";
-		echo "数据已保存，文件名是".$file.",请 进入data目录查看";
+		$this->putFiles($data);
 	}
 
 	/**
@@ -94,5 +60,67 @@ class AlibabaQuery {
 			echo "\r\n Fetching..., please wait...\r\n";
 			sleep(2);
 		}
+	}
+
+	/**
+	 * 对Url进行处理
+	 * @Author Foggy
+	 * @Date   2020-03-16
+	 * @WeChat [vita_hacker]
+	 * @Email  [x_foggy@163.com]
+	 * @param  [type]            $url [description]
+	 * @return [type]                 [description]
+	 */
+	protected function urlFormat(){
+		$urlParams = parse_url($this->inputUrl);
+		$domain = $urlParams['scheme'].'://'.$urlParams['host'];
+		//获取到结果一共有多少页
+		$urls = [];
+		$url = $html->find('.next-pagination-list a:eq(-1)')->href;
+		$url = $domain.$url;
+		$param = parse_url($url);
+		$ex = explode('/', $param['path']);
+		array_pop($ex);
+		$newParam = implode('/', $ex);
+		$param['path'] = $newParam;
+		for($i = 1; $i<= $count; $i++){
+			$urls[$i] = $param['scheme'].'://'.$param['host'].$param['path'].'/'.$i.'.html';
+		}
+		print "------------------------------\r\n";
+		print '搜索成功，本次搜索一共有'.count($urls).'页'."\r\n";
+		print "------------------------------\r\n";
+
+		return $urls;
+	}
+
+	/**
+	 * 写入文件
+	 * @Author Foggy
+	 * @Date   2020-03-16
+	 * @WeChat [vita_hacker]
+	 * @Email  [x_foggy@163.com]
+	 * @param  [type]            $content [description]
+	 * @return [type]                     [description]
+	 */
+	protected function putFiles($data){
+		$string = '';
+		ksort($data);
+		foreach ($data as $key => $value) {
+			$string .= '--------第'.$key.'页--------'."\r\n\r\n";
+			print '--------第'.$key.'页--------'."\r\n";;
+			foreach ($value as $k => $v) {
+				$string .= ($k+1).':'.$v."\r\n\r\n";
+				print ($k+1).':'.$v."\r\n";
+			}
+			print "\r\n";
+		}
+		$time = date('Y-m-d-H-i-s', time());
+		$file = './data/'.$time.'.txt';
+		if(!is_dir('./data')){
+			mkdir('./data/', 0755, true);
+		}
+		file_put_contents($file, $string);
+		print "------------------------------\r\n";
+		echo "数据已保存，文件名是".$file.",请 进入data目录查看";
 	}
 }
